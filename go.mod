c885fbf83a296a2eab0d860c425300cd3c209383
tonbi21 <sibuhiro81@gmail.com>
2022-07-13 17:35:06 +0900

    config create

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/tonbi21/go-test
+
+go 1.17
+
+require gopkg.in/go-ini/ini.v1 v1.66.6
+
+replace github.com/tonbi21/go-test/config => ./config
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "fmt"
+  "fmt"
+	"github.com/tonbi21/go-test/config"
 )
 
 func main() {
-    fmt.Println("テスト")
+  fmt.Println("テスト")
+	fmt.Println(config.Config.LogFile)
 }
